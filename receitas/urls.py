470bc22b5f13bd948865460ee4c5629cd72be706
fadrianomelo <f.adrianomelo@gmail.com>
2022-08-16 14:41:16 -0300

    Ajustando erro nas rotas

--- receitas/urls.py
+++ receitas/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-    path('receita', views.receita, name='receita')
+    path('', views.index),
+    path('receita', views.receita)
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
     
 def receita(request):
-    return render(request,'receita.html')
+    return render(request, 'receita.html')
 
 
 
